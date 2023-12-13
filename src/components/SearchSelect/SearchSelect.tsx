bf9160595159699255939d8aaca82a87fe1a3b49
Alan <pinhoneibor32@gmail.com>
2023-12-13 13:37:24 -0600

    feat: SearchSelect component created

--- src/components/SearchSelect/SearchSelect.tsx
+++ src/components/SearchSelect/SearchSelect.tsx
@@ -1,5 +1,15 @@
 export const SearchSelect = () => {
   return (
-    <div>SearchSelect</div>
+    <div className="select-container">
+      <select name="region-select" id="region-select">
+        <option value="">Filter by Region</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Antarctic">Antarctic</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </div>
   )
 }
