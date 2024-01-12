93a8f975c30230de1c58fdee4d74678f3932577a
Alan <pinhoneibor32@gmail.com>
2024-01-12 13:51:04 -0600

    feat: using CountriesContext in the useFetchCountryByName hook

--- src/hooks/useFetchCountryByName.ts
+++ src/hooks/useFetchCountryByName.ts
@@ -1,28 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { CountriesData, Translation } from '../types/CountriesTypes';
-import { getCountries } from '../helpers/getCountries';
 import { getCurrenciesValues } from '../helpers/getCurrenciesValues';
 import { getLanguagesValue } from '../helpers/getLanguagesValue';
 import { getNativeNamesValues } from '../helpers/getNativeNamesValues';
+import { CountriesContext } from '../context/CountriesContext';
 
 export const useFetchCountryByName = (countryName:string) => {
+  const { store } = useContext(CountriesContext);
 
   const [country, setCountry] = useState<CountriesData>({} as CountriesData);
   const [currencyName, setCurrencyName] = useState<string[]>([]);
   const [languagesName, setLanguagesName] = useState<(string | undefined)[]>();
   const [nativeName, setNativeName] = useState<Translation | undefined>();
 
-  const getCountryByName = async (name:string) => {
+  const getCountryByName = useCallback((name:string) => {
     const countryName = name.toLowerCase().split('-').join(' ');
-
-    const countriesInfo:CountriesData[] = await getCountries(`name/${countryName}`);
-
-    const countryData = countriesInfo.find(country => country.name.common.toLowerCase() === countryName);
+    
+    const countryData = store.countries.find(country => country.name.common.toLowerCase() === countryName);
     (countryData) && setCountry(countryData);
-  }
+  },[store])
+
+
   useEffect(() => {
     getCountryByName(countryName);
-  }, [countryName]);
+  }, [countryName, getCountryByName]);
 
   useEffect(() => {
     const {currencies, languages, name} = country;
