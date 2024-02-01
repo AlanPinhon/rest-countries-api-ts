c01f3587136a6dd1c8a739a27070da6f2f9395a0
Alan <pinhoneibor32@gmail.com>
2024-01-31 20:26:49 -0600

    style: theme color, loading text and error alert styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
 import Moon from '../../assets/images/moon.svg';
 import './HeaderStyles.css';
 
-export const Header = () => (
-  <header>
-    <h1 className="header-title">Where in the world?</h1>
-      <div className="icon-mode">
+export const Header = () => {
+
+  const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || 'light');
+  document.querySelector('body')?.setAttribute('data-theme', theme);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    document.querySelector('body')?.toggleAttribute('data-theme');
+    localStorage.setItem('theme', newTheme);
+  };
+  
+  return (
+    <header>
+      <h1 className="header-title">Where in the world?</h1>
+      <button onClick={toggleTheme} id='dark-mode-toggle' className="theme-mode-btn">
         <img src={Moon} className="icon"/>
-        <p className="theme-mode-text">Light Mode</p>
-      </div>
-  </header>
-)
+        <p className="theme-mode-text">Dark Mode</p>
+      </button>
+    </header>
+  )
+}
