d27190a01f51ff2aa9fe9d7a3a92080a20718fc7
Alan <pinhoneibor32@gmail.com>
2024-02-03 19:20:44 -0600

    feat: theme color with prefers-color-scheme and testing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Moon from '../../assets/images/moon.svg';
 import Sun from '../../assets/images/sun.svg';
 import './HeaderStyles.css';
 
 export const Header = () => {
-
-  const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || 'light');
+  //Get system color preference
+  const prefersDarkThemeMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  //Set initial theme based on system preference or local storage
+  const [theme, setTheme] = useState<string>(localStorage.getItem('theme') || (prefersDarkThemeMode ? 'dark' : 'light'));
   document.querySelector('body')?.setAttribute('data-theme', theme);
 
   const toggleTheme = () => {
@@ -15,11 +17,27 @@ export const Header = () => {
     localStorage.setItem('theme', newTheme);
   };
   
+  //Listen for changes to the system color preference
+  useEffect(() => {
+    const query = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const handleChange = () => {
+      setTheme(query.matches ? 'dark' : 'light');
+      document.querySelector('body')?.setAttribute('data-theme', theme);
+    };
+
+    query.addEventListener('change', handleChange);
+
+    return () => {
+      query.removeEventListener('change', handleChange);
+    };
+  }, [theme])
+  
   return (
     <header>
       <h1 className="header-title">Where in the world?</h1>
       <button onClick={toggleTheme} id='dark-mode-toggle' className="theme-mode-btn">
-        <img src={(theme === 'light') ? Moon : Sun} className="icon"/>
+        <img src={(theme === 'light') ? Moon : Sun} className="icon" alt="toggle-icon"/>
         <p className="theme-mode-text">
           {(theme === 'light') ? 'Dark' : 'Light'} Mode
         </p>
